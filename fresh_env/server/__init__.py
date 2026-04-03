894406f2db68073904b4b959a516ae64939ebfbc
CelestialWorthyOfHeavenAndEarth <aswinikumary@gmail.com>
2026-04-03 17:58:45 +0530

    Fix: Flatten openenv layout to root, add uv.lock to resolve multi-mode deployment failures

---
+++ fresh_env/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Fresh Env environment server components."""
+
+from .fresh_env_environment import FreshEnvironment
+
+__all__ = ["FreshEnvironment"]
